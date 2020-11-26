380dce20d560a23a2c8091ebd39f93d59730a96a
Max Zlobin <m@mzlobin.ru>
2020-11-26 17:42:24 +0300

    edit html templates and add base.html.

--- vacancies/add_in_db.py
+++ vacancies/add_in_db.py
@@ -8,8 +8,12 @@ from vacancies.data import companies, jobs, specialties
 from vacancies.models import Company, Vacancy, Specialty
 
 if __name__ == '__main__':
+    Company.objects.all().delete()
+    Specialty.objects.all().delete()
+    Vacancy.objects.all().delete()
     for company in companies:
         add_company = Company.objects.create(
+            id=company["id"],
             name=company["title"],
             location=company["location"],
             description=company["description"],
@@ -22,14 +26,15 @@ if __name__ == '__main__':
             title=special["title"]
         )
 
-#    for job in jobs:
-#       Vacancy.objects.create(
-#            title=job["title"],
-#            specialty=Specialty.objects.get(code=job["specialty"]),
-#            company=job["company"],
- #           skills=job["skills"],
-  #          description=job["description"],
-   #         salary_min=job["salary_from"],
-    #        salary_max=job["salary_to"],
-     #       published_at=job["posted"],
-      #  )
+    for job in jobs:
+        Vacancy.objects.create(
+            id=job["id"],
+            title=job["title"],
+            specialty=Specialty.objects.get(code=job["specialty"]),
+            company=Company.objects.get(id=job["company"]),
+            skills=job["skills"],
+            description=job["description"],
+            salary_min=job["salary_from"],
+            salary_max=job["salary_to"],
+            published_at=job["posted"],
+        )
