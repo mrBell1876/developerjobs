eb9b3526dfc923dc4f86fab180ddc09c8409dbd2
Max Zlobin <m@mzlobin.ru>
2020-11-30 15:28:10 +0300

    mod models and requirements

--- developerjobs/urls.py
+++ developerjobs/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from vacancies.views import custom_handler404, custom_handler500, MainView
+
 
 handler404 = custom_handler404
 handler500 = custom_handler500
@@ -26,3 +29,6 @@ urlpatterns = [
     path('', MainView.as_view(), name="main"),
     path('', include('vacancies.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- vacancies/models.py
+++ vacancies/models.py
@@ -1,20 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
+from developerjobs.settings import MEDIA_COMPANY_IMAGE_DIR, MEDIA_SPECIALITY_IMAGE_DIR
+
 
 class Company(models.Model):
     name = models.CharField(max_length=70)
     location = models.CharField(max_length=70)
-    logo = models.URLField(default='https://place-hold.it/100x60')
+    logo = models.ImageField(upload_to=MEDIA_COMPANY_IMAGE_DIR, height_field='height_field',
+                             width_field='width_field')
+    height_field = 100
+    width_field = 60
     description = models.TextField()
     employee_count = models.IntegerField()
+    owner = models.OneToOneField(User, null=True, on_delete=models.CASCADE, related_name='company')
+
 
 
 class Specialty(models.Model):
     code = models.CharField(max_length=20)
     title = models.CharField(max_length=70)
-    picture = models.URLField(default='https://place-hold.it/100x60')
+    picture = models.ImageField(upload_to=MEDIA_SPECIALITY_IMAGE_DIR, height_field='height_field',
+                                width_field='width_field')
+    height_field = 100
+    width_field = 60
 
 
 class Vacancy(models.Model):
@@ -26,3 +36,11 @@ class Vacancy(models.Model):
     salary_min = models.IntegerField()
     salary_max = models.IntegerField()
     published_at = models.DateField()
+
+
+class Application(models.Model):
+    written_username = models.CharField(max_length=20)
+    written_phone = models.IntegerField()
+    written_cover_letter = models.TextField()
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, related_name='applications')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='applications')
